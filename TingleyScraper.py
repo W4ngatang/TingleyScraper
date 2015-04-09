966241ae7134ff83a3e0e10d0f157c4a50d4c916
w4ngatang <alexwang@college.harvard.edu>
2015-04-09 00:13:29 -0400

    got the title too, able to full scrape a single page, need to dump into csv

--- TingleyScraper.py
+++ TingleyScraper.py
@@ -3,21 +3,23 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
 import urllib2
 
 base_url = "http://bbs.tianya.cn/post-worldlook-1432690-1.shtml"
 
 if __name__ == '__main__':
     page = urllib2.urlopen(base_url)
-    soup = beautifulsoup(page)
+    soup = BeautifulSoup(page)
 
     # print title of page
     # print url
-    firstflag = 0
+    print base_url
+    title = soup.find_all(class_='s_title')
+    print title[0].string
 
     # print time and contents
-    for post in soup.find_all(true, {'class':['bbs-content', 'atl-info']}):
+    for post in soup.find_all(True, {'class':['bbs-content', 'atl-info']}):
         if 'atl-info' in post['class']:
             print "time: ", post.contents[3].string[3:13]
             print "date: ", post.contents[3].string[14:]
