fdaca5e3d811eb961aea0778ec617e7b47705f0d
w4ngatang <alexwang@college.harvard.edu>
2015-04-08 23:52:29 -0400

    got time and date scraping, not to get title and will be done will single post

--- TingleyScraper.py
+++ TingleyScraper.py
@@ -3,25 +3,24 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-from bs4 import BeautifulSoup
+from bs4 import beautifulsoup
 import urllib2
 
-BASE_URL = "http://bbs.tianya.cn/post-worldlook-1432690-1.shtml"
+base_url = "http://bbs.tianya.cn/post-worldlook-1432690-1.shtml"
 
 if __name__ == '__main__':
-    page = urllib2.urlopen(BASE_URL)
-    soup = BeautifulSoup(page)
+    page = urllib2.urlopen(base_url)
+    soup = beautifulsoup(page)
 
     # print title of page
     # print url
+    firstflag = 0
 
     # print time and contents
-    for post in soup.find_all(True, {'class':['bbs-content', 'atl-info']}):
+    for post in soup.find_all(true, {'class':['bbs-content', 'atl-info']}):
         if 'atl-info' in post['class']:
-            print "New Post: "
-            for child in post.children:
-                if "时间" in child:
-                    print child.encode('utf-8')
+            print "time: ", post.contents[3].string[3:13]
+            print "date: ", post.contents[3].string[14:]
         elif 'bbs-content' in post['class']:
             for child in post.children:
                 contents = child.encode('utf-8')
