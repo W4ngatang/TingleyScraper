4e1c12a480cb7a87dcf50712f83d6f45ead84675
w4ngatang <alexwang@college.harvard.edu>
2015-04-09 00:33:55 -0400

    now dumping the scraping of the entirety of a single page of a post into a csv

--- TingleyScraper.py
+++ TingleyScraper.py
@@ -5,25 +5,36 @@ sys.setdefaultencoding("utf-8")
 
 from bs4 import BeautifulSoup
 import urllib2
+import csv
 
+outputFile = 'test.csv'
 base_url = "http://bbs.tianya.cn/post-worldlook-1432690-1.shtml"
 
 if __name__ == '__main__':
+
     page = urllib2.urlopen(base_url)
     soup = BeautifulSoup(page)
 
+    writer = csv.writer(open(outputFile, 'wb'), delimiter = ',')
+    writer.writerow(('Title', 'URL', 'Date', 'Time', 'Content'))
+
     # print title of page
     # print url
-    print base_url
-    title = soup.find_all(class_='s_title')
-    print title[0].string
+    url = base_url
+    title = soup.find_all(class_='s_title')[0].string
+    print title
 
     # print time and contents
     for post in soup.find_all(True, {'class':['bbs-content', 'atl-info']}):
+        content = ''
         if 'atl-info' in post['class']:
-            print "time: ", post.contents[3].string[3:13]
-            print "date: ", post.contents[3].string[14:]
+#            print "time: ", post.contents[3].string[3:13]
+#            print "date: ", post.contents[3].string[14:]
+            time = post.contents[3].string[3:13]
+            date = post.contents[3].string[14:]
         elif 'bbs-content' in post['class']:
             for child in post.children:
                 contents = child.encode('utf-8')
-                print contents.replace("<br/>", "").strip()
+#                print contents.replace("<br/>", "").strip()
+                content += contents.replace("<br/>", "").strip()
+            writer.writerow((title, url, date, time, content))
