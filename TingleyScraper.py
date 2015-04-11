7ef72bfdd5191e7c464db3975de274872f9b4782
w4ngatang <alexwang@college.harvard.edu>
2015-04-10 21:30:57 -0400

    need to test multipage scraping on threads

--- TingleyScraper.py
+++ TingleyScraper.py
@@ -10,6 +10,7 @@ import csv
 outputFile = 'test.csv'
 #base_url = "http://bbs.tianya.cn/post-worldlook-1432690-1.shtml"
 base_url = "http://search.tianya.cn/bbs?q=美国"
+iterator = "&pn="
 
 def scrapeSearch(url):
     page = urllib2.urlopen(url)
@@ -28,8 +29,10 @@ def scrapeThread(url):
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page)
    
+    # Get the title
     title = soup.find_all(class_='s_title')[0].string
    
+    # Get the contents of the posts   
     for post in soup.find_all(True, {'class':['bbs-content', 'atl-info']}):
         content = ''
         if 'atl-info' in post['class']:
@@ -43,41 +46,22 @@ def scrapeThread(url):
             print title,",",url,",",date,",",time,",",content
             print '\n'
 
+    # Get the next thread page if there is one
+    nextPage = soup.find_all(class_="js-keyboard-next"):
+    if nextPage != None:
+        scrapeThread(nextPage.get('href'))
+
+
 if __name__ == '__main__':
 
 #    writer = csv.writer(open(outputFile, 'wb'), delimiter = ',')
 #    writer.writerow(('Title', 'URL', 'Date', 'Time', 'Content'))
 
+    # go through all the next pages buttons, find the one with the largest value
+    # which should be the last page
+    counter = 1
     toScrape = scrapeSearch(base_url)
-    print "Done gathering URLs, scraping..."
 
     for target in toScrape:
-        scrapeThread(target)
+        result = scrapeThread(target)
 
-    '''
-    page = urllib2.urlopen(base_url)
-    soup = BeautifulSoup(page)
-
-    writer = csv.writer(open(outputFile, 'wb'), delimiter = ',')
-    writer.writerow(('Title', 'URL', 'Date', 'Time', 'Content'))
-
-    
-    # print title of page
-    # print url
-    url = base_url
-    title = soup.find_all(class_='s_title')[0].string
-    print title
-
-    # print time and contents
-    
-    for post in soup.find_all(True, {'class':['bbs-content', 'atl-info']}):
-        content = ''
-        if 'atl-info' in post['class']:
-            time = post.contents[3].string[3:13]
-            date = post.contents[3].string[14:]
-        elif 'bbs-content' in post['class']:
-            for child in post.children:
-                contents = child.encode('utf-8')
-                content += contents.replace("<br/>", "").strip()
-            writer.writerow((title, url, date, time, content))
-    '''
